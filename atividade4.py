1ae725034b46d8d06bd41cee0166db0b1c49ec08
Lila M. <lila.msfrazao@gmail.com>
2025-05-23 18:49:32 -0300

    Atividade 4

---
+++ atividade4.py
@@ -0,0 +1,16 @@
+# 4- Calculadora de Preço Total
+# * Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+# * Nome do produto: "Cadeira Infantil"
+# * Preço unitário: R$ 12.40
+# * Quantidade: 3
+# O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+
+#CTRL + K + C | CTRL + K + U
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+print(f"Nome do Produto: {nome_produto} | Preço Unitário: {preco_unitario} | Quantidade: {quantidade}\nPreço Total: {preco_total}")

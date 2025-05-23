528d161d2e02b2c3b430f529921a7e5e4f993497
Lila M. <lila.msfrazao@gmail.com>
2025-05-23 18:29:17 -0300

    Fizemos as atividades 1 a 3

---
+++ README.md
@@ -0,0 +1,1 @@
+# BRSAO179-Miniaula
---
+++ atividade3.py
@@ -0,0 +1,14 @@
+# 3- Calculadora de Volume
+# * Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+
+# * Comprimento: 12 cm
+# * Largura: 14 cm
+# * Altura: 20 cm
+# O programa deve calcular o volume e exibir o resultado em cm³.
+
+# comprimento x largura x altura
+comprimento = 12
+largura = 14
+altura = 20
+resultado = comprimento * largura * altura
+print(f"{resultado} cm³")
